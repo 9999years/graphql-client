cce7cdc720c42735ac8bd5140324a22b793a8e14
Rebecca Turner <rbt@sent.as>
2024-09-19 12:01:50 -0700

    Improve exception display for `GraphQLException` and `GraphQLError`
    Before:
    
        GraphQLException [GraphQLError {message = "Something went wrong!", locations = Just [GraphQLErrorLoc {errorLine = 10, errorCol = 3}], path = Just [String "my_query.graphql"], extensions = Just (Object (fromList [("complexity",Number 2.0)]))}]
    
    After:
    
        GraphQL errors:
        At 10:3: Something went wrong!
        Path: "my_query.graphql"
        Extensions: {"complexity":2}

--- graphql-client/src/Data/GraphQL/Error.hs
+++ graphql-client/src/Data/GraphQL/Error.hs
@@ -16,9 +16,12 @@ module Data.GraphQL.Error (
   GraphQLException (..),
 ) where
 
-import Control.Exception (Exception)
+import Control.Exception (Exception (..))
 import Data.Aeson (FromJSON (..), ToJSON, Value, withObject, (.:))
-import Data.Text (Text)
+import Data.Aeson.Text (encodeToLazyText)
+import Data.List (intersperse)
+import Data.Text (Text, unpack)
+import Data.Text.Lazy (toStrict)
 import GHC.Generics (Generic)
 
 -- | An error in a GraphQL query.
@@ -30,12 +33,30 @@ data GraphQLError = GraphQLError
   }
   deriving (Show, Eq, Generic, ToJSON, FromJSON)
 
+instance Exception GraphQLError where
+  displayException exception =
+    "At "
+      <> joinWithCommas (maybe [] (map displayErrorLoc) (locations exception))
+      <> ": "
+      <> unpack (message exception)
+      <> maybe "" (("\nPath: " <>) . joinWithCommas . map valueToString) (path exception)
+      <> maybe "" (("\nExtensions: " <>) . valueToString) (extensions exception)
+    where
+      joinWithCommas = unwords . intersperse ", "
+
+      valueToString :: Value -> String
+      valueToString = unpack . toStrict . encodeToLazyText
+
 -- | A location in an error in a GraphQL query.
 data GraphQLErrorLoc = GraphQLErrorLoc
   { errorLine :: Int
   , errorCol :: Int
   }
   deriving (Show, Eq, Generic, ToJSON)
+
+displayErrorLoc :: GraphQLErrorLoc -> String
+displayErrorLoc errorLoc =
+  show (errorLine errorLoc) <> ":" <> show (errorCol errorLoc)
 
 instance FromJSON GraphQLErrorLoc where
   parseJSON = withObject "GraphQLErrorLoc" $ \o ->
@@ -45,4 +66,9 @@ instance FromJSON GraphQLErrorLoc where
 
 -- | An exception thrown as a result of an error in a GraphQL query.
 newtype GraphQLException = GraphQLException [GraphQLError]
-  deriving (Show, Exception, Eq)
+  deriving (Show, Eq)
+
+instance Exception GraphQLException where
+  displayException (GraphQLException errors) =
+    "GraphQL errors:\n"
+      <> concat (intersperse "\n\n" $ map displayException errors)
